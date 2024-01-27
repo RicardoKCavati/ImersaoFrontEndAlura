549de6aeb7c67968b3b87635244a54093c7bbf81
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2024-01-27 14:15:04 -0300

    Adicionar Main à página principal.

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Sidebar from './Sidebar/Sidebar';
+import Main from './Main/Main';
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
       <Sidebar />
       <Footer />
     </div>
